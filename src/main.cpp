643377c2b7b1012a84bc268097f789c0271acf8e
Daniel Calderon <21376367+dantros@users.noreply.github.com>
2024-10-28 23:02:05 -0300

    Removing gamepad support as it was buggy. Adding shadows and materials.
    Squashed commit of the following:
    
    commit 0bd91922fd2f44309775ca341534ebf6cc52e03e
    Author: Daniel Calderon <21376367+dantros@users.noreply.github.com>
    Date:   Mon Oct 28 23:00:42 2024 -0300
    
        a few touches
    
    commit f01deef7d0bf8e70af69c03f50b49ad83ef625fd
    Author: Daniel Calderon <21376367+dantros@users.noreply.github.com>
    Date:   Mon Oct 28 22:30:00 2024 -0300
    
        2 shadows working. There are many combinatrions that do not work. There are no error messages :( .
    
    commit 655d514ab3691d5f043c010966df2c821660bb18
    Author: Daniel Calderon <21376367+dantros@users.noreply.github.com>
    Date:   Mon Oct 28 20:02:42 2024 -0300
    
        tidying assets
    
    commit 7eeb658769fa163c9944f29f0f1ea74e7ddc91a4
    Author: Daniel Calderon <21376367+dantros@users.noreply.github.com>
    Date:   Mon Oct 28 19:14:30 2024 -0300
    
        removing spare assets
    
    commit c4f0c5547e5e4e65c45356c9db3dca797d09ca9f
    Author: Daniel Calderon <21376367+dantros@users.noreply.github.com>
    Date:   Mon Oct 28 19:13:51 2024 -0300
    
        relocating screenshot
    
    commit e8adf97dce2da15fdbe8c8b212bf69bc10559904
    Author: Daniel Calderon <21376367+dantros@users.noreply.github.com>
    Date:   Mon Oct 28 18:38:03 2024 -0300
    
        adding basic color materials
    
    commit dfdbb8c2b7d716c2831f82605ad6099d4a52a224
    Author: Daniel Calderon <21376367+dantros@users.noreply.github.com>
    Date:   Mon Oct 28 18:13:01 2024 -0300
    
        installing config files
    
    commit a92a96b5e69d6af750cb9a11a6529a6c8483d79e
    Author: Daniel Calderon <21376367+dantros@users.noreply.github.com>
    Date:   Mon Oct 28 17:45:16 2024 -0300
    
        improvement to configuration files (still not being used)
    
    commit 11f027fd5739a89189094e989156f8b29996167b
    Author: Daniel Calderon <21376367+dantros@users.noreply.github.com>
    Date:   Mon Oct 28 17:42:39 2024 -0300
    
        using a quad as a background object for shadow projection
    
    commit a29952f11139e35a7b7604e56efeb46d546b45e9
    Author: Daniel Calderon <21376367+dantros@users.noreply.github.com>
    Date:   Mon Oct 28 17:39:23 2024 -0300
    
        moving the paddle continuously while the keys are pressed
    
    commit 8d35748235f3995dcf8bab3096435280c9e1fc1a
    Author: Daniel Calderon <21376367+dantros@users.noreply.github.com>
    Date:   Mon Oct 28 04:01:13 2024 -0300
    
        reducing Ogre WIP
    
    commit 4887701bb9dfeec91be481f086aedded556cc837
    Author: Daniel Calderon <21376367+dantros@users.noreply.github.com>
    Date:   Wed Oct 23 17:29:56 2024 -0300
    
        adding shadows and materials [WIP]

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
-#include "Ogre.h"
-#include "OgreApplicationContext.h"
-#include "OgreInput.h"
-#include "OgreRTShaderSystem.h"
+#include <Ogre.h>
+#include <OgreApplicationContext.h>
+#include <OgreInput.h>
+#include <OgreRTShaderSystem.h>
+#include <OgreFileSystemLayer.h>
+//#include <iostream>
 #include "ball.h"
 #include "brick.h"
 #include "player.h"
@@ -14,6 +16,10 @@ public:
 
     void setup();
     bool keyPressed(const OgreBites::KeyboardEvent& evt);
+private:
+    int backgroundMaterialId = 0;
+    std::vector<Ogre::String> backgrounds;
+    Ogre::Entity* backgroundEntity;
 };
 
 Breakout::Breakout() : OgreBites::ApplicationContext("Breakout")
@@ -23,22 +29,54 @@ Breakout::Breakout() : OgreBites::ApplicationContext("Breakout")
 void Breakout::setup()
 {
     // Calling the base first, adding the input listener
+    // This will create a file ogre.cfg at your OS user folder/Breakout. Example: My Documents/Breakout.
+    // that file contains your initial setup, such as window size, full screen, render backend, etc.
+    // This is the way OgreBites work, if we do not want this, we need to implement something equivalent
+    // to OgreBites.
+    // *Important Note*: OgreBites was only meant to be used with Ogre examples, so it is very incomplete
+    // for general purposes. Example: At the time of writting, joystick analog inputs are not properly
+    // detected (all axis are showing the same event parameters for some reason).
     OgreBites::ApplicationContext::setup();
+
+    // Adding an input listener
     addInputListener(this);
 
     // Pointer to the default root
     Ogre::Root* root = getRoot();
     Ogre::SceneManager* scnMgr = root->createSceneManager();
+    scnMgr->setAmbientLight(Ogre::ColourValue(0, 0, 0));
+
+    // Use Ogre's custom shadow mapping ability
+    Ogre::MaterialManager* materialMgr = Ogre::MaterialManager::getSingletonPtr();
+    scnMgr->setShadowTexturePixelFormat(Ogre::PF_DEPTH16);
+    // only MODULATIVE technique is working for some reason.
+    //scnMgr->setShadowTechnique(Ogre::ShadowTechnique::SHADOWTYPE_TEXTURE_ADDITIVE);
+    //scnMgr->setShadowTechnique(Ogre::ShadowTechnique::SHADOWTYPE_TEXTURE_ADDITIVE_INTEGRATED);
+    scnMgr->setShadowTechnique(Ogre::ShadowTechnique::SHADOWTYPE_TEXTURE_MODULATIVE);
+    //scnMgr->setShadowTechnique(Ogre::ShadowTechnique::SHADOWTYPE_TEXTURE_MODULATIVE_INTEGRATED);
+
+    // default is only 1 shadow map
+    scnMgr->setShadowTextureCount(5);
+    scnMgr->setShadowTextureCasterMaterial(materialMgr->getByName("PSSM/shadow_caster"));
+    scnMgr->setShadowTextureReceiverMaterial(materialMgr->getByName("Ogre/DepthShadowmap/Receiver/RockWall"));
+    // for shadow mapping, selkf shadowing is not implemented by default.
+    scnMgr->setShadowTextureSelfShadow(false);
+    scnMgr->setShadowTextureSize(1024);
 
     // Registering the scene with the RTShaderSystem
     Ogre::RTShader::ShaderGenerator* shadergen = Ogre::RTShader::ShaderGenerator::getSingletonPtr();
     shadergen->addSceneManager(scnMgr);
 
+//#if 0
     // A light source is needed
     Ogre::Light* light = scnMgr->createLight("MainLight");
+    light->setType(Ogre::Light::LightTypes::LT_POINT);
+    light->setCastShadows(true);
     Ogre::SceneNode* lightNode = scnMgr->getRootSceneNode()->createChildSceneNode();
-    lightNode->setPosition(0, 10, 15);
+    lightNode->setDirection(0,1,-1);
+    lightNode->setPosition(0, 0, 20);
     lightNode->attachObject(light);
+//#endif
 
     // Setting up the camera
     Ogre::Camera* cam = scnMgr->createCamera("myCam");
@@ -52,24 +90,36 @@ void Breakout::setup()
     // Setting up the viewport
     getRenderWindow()->addViewport(cam);
 
-    // Setting up the bricks
-    // First, we add the local directory to the resource group manager
-    Ogre::ResourceGroupManager::getSingleton().addResourceLocation("assets", "FileSystem");
-    Ogre::ResourceGroupManager::getSingleton().initialiseAllResourceGroups();
-
-    // Array of 20 entities
-    Ogre::Entity* bricks[21];
-    // Array of 20 scene nodes
+    // 20 bricks + 3 walls + paddle
+    Ogre::Entity* bricks[24];
     Ogre::SceneNode* brickNodes[24];
-    // Array of 20 bricks
     BreakoutBrick* bricksActors[24];
-    // Array of mesh names
-    Ogre::String meshNames[4] = {"purple.obj", "red.obj", "yellow.obj", "green.obj"};
+
+    std::vector<Ogre::String> meshMaterials{
+        "Basics/Red",
+        "Basics/Green",
+        "Basics/Blue",
+        "Basics/Yellow",
+        "Basics/Purple",
+        "Basics/Aqua"
+    };
+
+    backgrounds = {
+        "Advanced/ScrollingClouds",
+        "Advanced/ScrollingColor",
+        "Basics/Gray",
+        "Basics/Black",
+        "Advanced/MagicChrome",
+        "Advanced/UglyWater",
+        "Advanced/Rocks"
+    };
 
     // Loop to create the entities and scene nodes
     for (int i = 0; i < 20; i++)
     {
-        bricks[i] = scnMgr->createEntity("Brick" + Ogre::StringConverter::toString(i), meshNames[i / 5]);
+        bricks[i] = scnMgr->createEntity("Brick" + Ogre::StringConverter::toString(i), "brick.obj");
+        bricks[i]->getSubEntity(0)->setMaterialName(meshMaterials[i % meshMaterials.size()]);
+        bricks[i]->setCastShadows(true); // true is the default anyways.
         brickNodes[i] = scnMgr->getRootSceneNode()->createChildSceneNode("Brick" + Ogre::StringConverter::toString(i) + "Node");
         bricksActors[i] = new BreakoutBrick(brickNodes[i]);
         bricksActors[i]->setup(7.5, 2.0);
@@ -95,14 +145,25 @@ void Breakout::setup()
     bricksActors[22]->setPos(Ogre::Vector3(0, 17.0, 0));
 
     // Setting up the player brick
-    bricks[20] = scnMgr->createEntity("Player", "blue.obj");
+    bricks[23] = scnMgr->createEntity("Player", "brick.obj");
+    bricks[23]->getSubEntity(0)->setMaterialName("Advanced/UnlitWhite");
     brickNodes[23] = scnMgr->getRootSceneNode()->createChildSceneNode("PlayerNode");
     BreakoutPlayer* player = new BreakoutPlayer(brickNodes[23]);
     bricksActors[23] = player;
     bricksActors[23]->setup(7.5, 2.0);
     bricksActors[23]->setPos(Ogre::Vector3(0, -11.0, 0));
     brickNodes[23]->yaw(Ogre::Degree(90));
-    brickNodes[23]->attachObject(bricks[20]);
+    brickNodes[23]->attachObject(bricks[23]);
+
+    // Background
+    backgroundEntity = scnMgr->createEntity("Background", "quad.obj");
+    backgroundEntity->getSubEntity(0)->setMaterialName("Advanced/ScrollingClouds");
+    backgroundEntity->setCastShadows(false);
+    Ogre::SceneNode* backgroundNode = scnMgr->getRootSceneNode()->createChildSceneNode("BackgroundNode");
+    backgroundNode->setPosition(Ogre::Vector3(0, 0, -5));
+    backgroundNode->setOrientation(Ogre::Quaternion(Ogre::Degree(90), Ogre::Vector3(1,0,0)));
+    backgroundNode->setScale(2.5,1,2.5);
+    backgroundNode->attachObject(backgroundEntity);
 
     // Create the BreakoutPlayer and attach it to the root
     addInputListener(player);
@@ -110,10 +171,24 @@ void Breakout::setup()
 
     // Create a sphere entity and node
     Ogre::Entity* sphereEntity = scnMgr->createEntity("Sphere", "ball.obj");
+    sphereEntity->getSubEntity(0)->setMaterialName("Advanced/EnvMappedRustySteel");
     Ogre::SceneNode* sphereNode = scnMgr->getRootSceneNode()->createChildSceneNode("SphereNode");
     sphereNode->setPosition(0, -9.0, 0);
     sphereNode->yaw(Ogre::Degree(90));
     sphereNode->attachObject(sphereEntity);
+
+//#if 0
+    // second light source, not all combinations work, no error, so be sure it is working when modifying it.
+    Ogre::Light* light2 = scnMgr->createLight("SecondaryLight");
+    light2->setType(Ogre::Light::LightTypes::LT_POINT);
+    light2->setCastShadows(true);
+    light2->setAttenuation(50.0, 0.5, 0.0001, 0.0);
+    Ogre::SceneNode* light2Node = scnMgr->getRootSceneNode()->createChildSceneNode();
+    //Ogre::SceneNode* light2Node = brickNodes[23]->createChildSceneNode();
+    light2Node->setDirection(0, 0, -1);
+    light2Node->setPosition(10, 0, 20);
+    light2Node->attachObject(light2);
+//#endif
 
     // Create the BreakoutBall and attach it to the root
     BreakoutBall* breakoutBall = new BreakoutBall(sphereNode, bricksActors, 24);
@@ -125,6 +200,11 @@ void Breakout::setup()
 
 bool Breakout::keyPressed(const OgreBites::KeyboardEvent& evt)
 {
+    if (evt.keysym.sym == OgreBites::SDLK_SPACE)
+    {
+        backgroundMaterialId = backgroundMaterialId + 1 < backgrounds.size() ? backgroundMaterialId + 1 : 0;
+        backgroundEntity->getSubEntity(0)->setMaterialName(backgrounds.at(backgroundMaterialId));
+    }
     if (evt.keysym.sym == OgreBites::SDLK_ESCAPE)
     {
         getRoot()->queueEndRendering();
